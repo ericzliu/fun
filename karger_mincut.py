c36aa485c7b82e19e7015ec09d91dfb185839b9a
Zhi LIU <liuzhi0210@gmail.com>
2017-07-29 01:44:39 +0200

    Fix contraction step.
    The contraction step should maintain the invariant on the graph:
    each node contains all in and out edges from it.

--- karger_mincut.py
+++ karger_mincut.py
@@ -5,23 +5,50 @@ Created on Thu Jul 27 23:10:23 2017
 @author: DIAMIAO
 """
 import random
+import unittest
 
 class Node(object):
     def __init__(self):
         self.id = 0
         self.arcs = []
         self.node_set = set()
+    def __str__(self):
+        return 'Node (' + str(self.id) + ')'
 
 class Arc(object):
     def __init__(self, node1, node2):
         self.first = node1
         self.second = node2
+    def __str__(self):
+        return 'Arc (' + str(self.first.id) + ', ' + str(self.second.id) + ')'
+
+class Random(object):
+    def __init__(self, seed):
+        random.seed(seed)
+
+    def randint(self, first, last):
+        random.randint(first, last)
+
+class AlwaysFirst(object):
+    def __init__(self):
+        pass
+
+    def randint(self, first, last):
+        return first
+
+class AlwaysLast(object):
+    def __init__(self):
+        pass
+
+    def randint(self, first, last):
+        return last
 
 class MinCut(object):
-    def __init__(self):
+    def __init__(self, rand):
         self._nodes = []
-        self._arcs = set()
+        self._arcs = []
         self._edges = set()
+        self._rand = rand
 
     def _get_node(self, nid):
         for node in self._nodes:
@@ -33,20 +60,30 @@ class MinCut(object):
         self._nodes.append(new_node)
         return new_node
 
+    def _get_arc(self, node1, node2):
+        for arc in self._arcs:
+            if arc.first.id == node1.id and arc.second.id == node2.id:
+                return arc
+        arc = Arc(node1, node2)
+        self._arcs.append(arc)
+        return arc
+
     def _read_single(self, node_list):
         if len(node_list) > 1:
-            nid1 = node_list[0]
-            node1 = self._get_node(nid1)
+            nid = node_list[0]
+            node = self._get_node(nid)
             for nid2 in node_list:
-                if nid2 == nid1:
+                if nid2 == nid:
                     continue
                 node2 = self._get_node(nid2)
-                arc = Arc(node1, node2)
-                edge = (nid1, nid2) if nid1 < nid2 else (nid2, nid1)
-                self._edges.add(edge)
-                self._arcs.add(arc)
-                node1.arcs.add(arc)
-                node2.arcs.add(arc)
+                nid1 = nid
+                node1 = node
+                if nid1 > nid2:
+                    node1, node2 = node2, node1
+                    nid1, nid2 = nid2, nid1
+                arc = self._get_arc(node1, node2)
+                node.arcs.append(arc)
+                self._edges.add((nid1, nid2))
 
     def read_file(self, name):
         with open(name) as file:
@@ -55,44 +92,87 @@ class MinCut(object):
                 self._read_single(node_list)
 
     def min_cut(self):
+        self._print_graph()
         while len(self._nodes) > 2 and len(self._arcs) > 0:
             self._contraction()
         if len(self._nodes) == 2:
-            return self.count_cut()
+            return self._count_cut()
         return 0
 
     def _contraction(self):
-        ind = random.randint(0, len(self._arcs) - 1)
+        ind = self._rand.randint(0, len(self._arcs) - 1)
         arc_to_shrink = self._arcs[ind]
         node_to_keep = arc_to_shrink.first
         node_to_delete = arc_to_shrink.second
         if node_to_keep.id > node_to_delete.id:
             node_to_keep, node_to_delete = node_to_delete, node_to_keep
-        arcs_to_delete = []
-        self._update_arcs(node_to_keep, node_to_delete, arcs_to_delete)
-        node_to_keep.node_set.update(node_to_delete.node_set)
-        self._arcs.difference_update(arcs_to_delete)
-        self._nodes.remove(node_to_delete)
+        self._update_graph(node_to_keep, node_to_delete)
+        self._print_graph()
 
-    def _update_arcs(self, first, second, arcs_to_delete):
+    @staticmethod
+    def _remove_self_arcs(_arcs):
+        j = len(_arcs) - 1
+        while j >= 0:
+            arc = _arcs[j]
+            if arc.first.id == arc.second.id:
+                j -= 1
+            else:
+                break
+
+        i = 0
+        while i < j:
+            arc = _arcs[i]
+            if arc.first.id == arc.second.id:
+                _arcs[i], _arcs[j] = _arcs[j], _arcs[i]
+                j -= 1
+            else:
+                i += 1
+
+        while len(_arcs) > 0:
+            last = _arcs[len(_arcs) - 1]
+            if last.first.id == last.second.id:
+                _arcs.pop()
+            else:
+                break
+
+    def _update_graph(self, first, second):
+
+        # Replace the second node by first node in edges
         for arc in second.arcs:
             if arc.first.id == second.id:
                 arc.first = first
-            elif arc.second.id == second.id:
+            if arc.second.id == second.id:
                 arc.second = first
-            if arc.first.id == arc.second.id:
-                arcs_to_delete.append(arc)
+        # edges pointed by second node now should be pointed by first point
+        # invariant: each node contains a list of all the in and out edges
+        first.arcs.extend(second.arcs)
+
+        MinCut._remove_self_arcs(first.arcs)
+        MinCut._remove_self_arcs(self._arcs)
+        first.node_set.update(second.node_set)
+        self._nodes.remove(second)
 
     def _count_cut(self):
-        a = self._nodes[0].node_set
-        b = self._nodes[1].node_set
-        num_cut = 0
-        for n1 in a:
-            for n2 in b:
-                e = (n1, n2) if n1 < n2 else (n2, n1)
-                if e in self._edges:
-                    num_cut += 1
-        return num_cut
+        return len(self._arcs)
+
+    def _print_graph(self):
+        print('==============================================================')
+        for node in self._nodes:
+            print(node)
+        for arc in self._arcs:
+            print(arc)
+
+class TestMinCut(unittest.TestCase):
+    def test_complete_4_nodes(self):
+        rand = AlwaysFirst()
+        mincut = MinCut(rand)
+        mincut.read_file('complete_4.txt')
+        self.assertEqual(4, mincut.min_cut())
+
+        rand = AlwaysLast()
+        mincut = MinCut(rand)
+        mincut.read_file('complete_4.txt')
+        self.assertEqual(3, mincut.min_cut())
 
 if __name__=='__main__':
-    pass
+    unittest.main()
